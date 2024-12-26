b1b0bc0a6e208226023a4ae086110daa890b9191
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-12-25 19:06:45 -0500

    Time: 116 ms (78.15%), Space: 17.9 MB (63.39%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1
+        
+        for num in nums:
+            next_dp = defaultdict(int)
+            for current_sum, ways in dp.items():
+                next_dp[current_sum + num] += ways
+                next_dp[current_sum - num] += ways
+            dp = next_dp
+
+        return dp[target]
