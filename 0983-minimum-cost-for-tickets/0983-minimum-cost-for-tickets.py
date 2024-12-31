bd1e3a2a5451eb3d54bc249950b493716d9da9cd
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-12-30 19:28:21 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        def dfs(idx):
+            if idx == len(days):
+                return 0
+            return min(
+                costs[0] + dfs(bisect_left(days, days[idx] + 1)),
+                costs[1] + dfs(bisect_left(days, days[idx] + 7)),
+                costs[2] + dfs(bisect_left(days, days[idx] + 30)),
+            )
+
+        return dfs(0)
