fe90bf266afbea953078541b7af00157b83f0007
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-02-16 11:31:45 -0500

    Time: 10 ms (88.39%), Space: 57.5 MB (69.92%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> unique = new HashSet<>();
+
+        for (int num : nums) {
+            if (unique.contains(num)) {
+                return true;
+            }
+            unique.add(num);
+        }
+        return false;
+    }
+}
