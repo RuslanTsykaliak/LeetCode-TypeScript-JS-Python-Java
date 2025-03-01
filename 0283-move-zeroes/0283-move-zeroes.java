c8d350ab9b1cbe951df89c1ada0da68b0a977b91
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-02-28 19:54:37 -0500

    Time: 3 ms (22.75%), Space: 45.8 MB (79.7%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for (int i = 0, j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                if (i < j) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+                i++;
+            }
+        }
+    }
+}
