ad31f3706be93be741afe6f42aeab92867a7fa5a
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-03-28 09:38:33 -0400

    Time: 586 ms (67.97%), Space: 42.5 MB (28.76%) - LeetHub

---
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+        # Algorithms: Heap-based (Priority Queue) Search
+        # Using a min-heap allows you to efficiently explore the grid cells in ascending order of their values. 
+        # Sorting the queries allows you to process them incrementally, avoiding redundant calculations.
+        
+        ROWS, COLS = len(grid), len(grid[0])
+
+        q = [(n, i) for i, n in enumerate(queries)]
+        q.sort()
+
+        min_heap = [(grid[0][0], 0, 0)] # val, r, c
+        visit = set([(0, 0)])
+        res = [0] * len(queries)
+        points = 0
+
+        for limit, index in q:
+            while min_heap and min_heap[0][0] < limit:
+                val, r, c, = heapq.heappop(min_heap)
+                points += 1
+                neighbors = [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]
+                for nr, nc in neighbors:
+                    if (
+                        0 <= nr < ROWS and 0 <= nc < COLS and
+                        (nr, nc) not in visit
+                    ):
+                        heapq.heappush(min_heap, (grid[nr][nc], nr, nc))
+                        visit.add((nr, nc))
+            res[index] = points
+        return res
