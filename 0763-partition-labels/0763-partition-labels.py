77a217d21492fe29aaf2609ef037a9bf1e7ec4f7
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-03-30 09:08:41 -0400

    Time: 3 ms (91.15%), Space: 17.9 MB (36.45%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,26 +1,26 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        # Input: String
-        # Constrains: split String so that each letter appears only in one part
-        # Output: a list of integers representig the size of these parts
-        # Use Hash or array of characters to be able check what character present
-        # We can use Sliding Window by creating start and end. The condition of spliting a string from left side to the right and moving whole window and ensure no character in the current window appears in any other partition
+        # Algorithms: Two Pointers with HashMap & Greedy
 
-        # Sliding Window
-
-        # Calculate the last index
-        last_index = {char: idx for idx, char in enumerate(s)}
-
-        partitions = []
+        # 1. variables
+        res = []
         start, end = 0, 0
 
-        for i, char in enumerate(s):
-            # Expand the partition
-            end = max(end, last_index[char])
-            # End of the current partition
-            if i == end:
-                partitions.append(end - start + 1)
-                # Start of the next partition
-                start = i + 1
+        # 2. HashMap with the last index of the character
+        # For example, a:2, b:3, c:5
+        last_index = {char: idx for idx, char in enumerate(s)}
 
-        return partitions
+        # Iterate over the string
+        for i, char in enumerate(s):
+            end = max(end, last_index[char]) # end for a will be 2, end for b to 3
+
+            if i == end:
+                res.append(end - start + 1)
+                start = i + 1
+                
+        return res
+
+
+
+
+        
