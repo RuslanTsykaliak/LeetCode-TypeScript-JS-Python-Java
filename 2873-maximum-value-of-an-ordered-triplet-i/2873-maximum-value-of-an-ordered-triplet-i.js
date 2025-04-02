dd961c1a629052227e7167973ca4a1dba5306fa3
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-02 09:46:44 -0400

    Time: 1 ms (83.78%), Space: 55.8 MB (20.27%) - LeetHub

---
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumTripletValue = function(nums) {
+    let res = 0, left = 0, diff = 0
+
+    for (const i of nums) {
+        res = Math.max(res, diff * i)
+        diff = Math.max(diff, left - i)
+        left = Math.max(left, i)
+    }
+    return res
+};
