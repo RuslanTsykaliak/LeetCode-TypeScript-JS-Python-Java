da414222668427e581a1ad6f704505732cd4b39c
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-03 09:29:36 -0400

    Time: 169 ms (74.4%), Space: 29.4 MB (67.92%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.py
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.py
@@ -1,9 +1,16 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
+        # Algorithm: Greedy
+
+        # Variables
         res, diff, left = 0, 0, 0
 
-        for i in nums:
-            res = max(res, diff * i)
-            diff = max(diff, left - i)
-            left = max(left, i)
+        # For loop
+        for k in nums:
+            res = max(res, diff * k)
+            diff = max(diff, left - k)
+            left = max(left, k)
         return res
+
+
+        
