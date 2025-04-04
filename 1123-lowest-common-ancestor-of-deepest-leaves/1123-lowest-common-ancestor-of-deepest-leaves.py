647e42944f1335a848ac736ef856e36521391c51
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-03 23:13:12 -0400

    Time: 4 ms (37.41%), Space: 18.4 MB (25.86%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -6,23 +6,44 @@
 #         self.right = right
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # Algorithm: Binary Search (most likely). DFS. BFS
+        # Algorithms: DFS. BFS. Can we try  Binary Search?
         # Input: the root of a binary tree
         # Output: the lowest common ancestor of its deepest leaves: The lowes elements in the binary tree
 
         # Recursion
-        def dfs(root):
-            if not root:
-                return 0, None
+        # To-Do add explaining comments
+        # def dfs(root):
+        #     if not root:
+        #         return 0, None
             
-            left = dfs(root.left)
-            right = dfs(root.right)
+        #     left = dfs(root.left)
+        #     right = dfs(root.right)
             
-            if left[0] > right[0]:
-                return left[0] + 1, left[1]
-            if left[0] < right[0]:
-                return right[0] + 1, right[1]
-            return left[0] + 1, root
+        #     if left[0] > right[0]:
+        #         return left[0] + 1, left[1]
+        #     if left[0] < right[0]:
+        #         return right[0] + 1, right[1]
+        #     return left[0] + 1, root
         
-        return dfs(root)[1]
+        # return dfs(root)[1]
+
+        # Algorithm: Depth First Search
+        # To-do add explaining comments
+        def dfs(node, depth):
+            if not node:
+                return (None, depth + 1)
+
+            left_node, left_depth = dfs(node.left, depth + 1)
+            right_node, right_depth = dfs(node.right, depth + 1)
+
+            if left_depth > right_depth:
+                return left_node, left_depth
+            elif left_depth < right_depth:
+                return right_node, right_depth
+            return node, left_depth
+
+        node, _ = dfs(root, 0)
+        return node
+
+
         
