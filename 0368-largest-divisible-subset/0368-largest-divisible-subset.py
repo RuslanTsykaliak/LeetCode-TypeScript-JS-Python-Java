0f0590d970c62aa6c873cfab58bf4ae67345fafd
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-06 14:47:37 -0400

    Time: 184 ms (71.52%), Space: 17.9 MB (76.77%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,26 +1,22 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
-        res = collections.defaultdict(list)
-        res[1].append([nums[0]])
-        max_len = 1
+        n = len(nums)
+        dp = [1] * n
+        prev = [-1] * n
+        max_idx = 0
 
-        for i in range(1, len(nums)):
-            num = nums[i]
-            added = False
-            for length in range(max_len, 0, -1):
-                for path in res[length]:
-                    if num % path[-1] == 0:
-                        new_path = path + [num]
-                        new_len = length + 1
-                        res[new_len].append(new_path)
-                        max_len = max(max_len, new_len)
-                        # if new_len > max_len:
-                        #     max_len = new_len
-                        added = True
-                        break
-                if added:
-                    break
-            if not added:
-                res[1].append([num])
-        return res[max_len][0]
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and dp[j] + 1 > dp[i]:
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+            if dp[i] > dp[max_idx]:
+                max_idx = i
+
+        # Reconstruct the path
+        res = []
+        while max_idx != -1:
+            res.append(nums[max_idx])
+            max_idx = prev[max_idx]
+        return res[::-1]
