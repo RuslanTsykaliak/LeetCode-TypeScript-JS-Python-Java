3ac32cc97530184ded993a1fe30977f8be95cbf9
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-06 14:37:53 -0400

    Time: 103 ms (99.91%), Space: 18.2 MB (39.18%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,33 +1,26 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        nums.sort()  # Sort the numbers to check divisibility easily
-
-        # res: Key = path length, Value = list of paths of that length
+        nums.sort()
         res = collections.defaultdict(list)
-        res[1].append([nums[0]])  # First number starts a path of length 1
-        max_len = 1  # Track the length of the longest path found
+        res[1].append([nums[0]])
+        max_len = 1
 
         for i in range(1, len(nums)):
-            num = nums[i]  # Current number to potentially add
-            added = False  # Track if 'num' was added to any path
-
-            # Check existing paths, from longest to shortest
+            num = nums[i]
+            added = False
             for length in range(max_len, 0, -1):
                 for path in res[length]:
-                    if num % path[-1] == 0:  # Check divisibility rule
-                        new_path = path + [num]  # Add 'num' to the path
+                    if num % path[-1] == 0:
+                        new_path = path + [num]
                         new_len = length + 1
-                        res[new_len].append(new_path)  # Record the new path
-
-                        if new_len > max_len:  # Update max length found
-                            max_len = new_len
-
+                        res[new_len].append(new_path)
+                        max_len = max(max_len, new_len)
+                        # if new_len > max_len:
+                        #     max_len = new_len
                         added = True
-                        break  # Stop checking paths at this length
+                        break
                 if added:
-                    break  # Stop checking shorter lengths
-
-            if not added:  # If 'num' couldn't extend any path
-                res[1].append([num])  # Start a new path of length 1
-
-        return res[max_len][0]  # Return any path with the maximum length
+                    break
+            if not added:
+                res[1].append([num])
+        return res[max_len][0]
