467427f1db3c4b58a0fc88924b929ee9055338ce
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-06 11:43:18 -0400

    Time: 211 ms (26.91%), Space: 17.9 MB (76.77%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -5,17 +5,49 @@ class Solution:
         # Constrains answer[i] % answer[j] == 0
 
         # Algorithm: Dynamic Programming
-        nums.sort()  # Sorting to make divisibility checks easier
-        N = len(nums)
-        dp = [[num] for num in nums]  # Initialize subsets with single elements
-        max_subset = []
+        # nums.sort()  # Sorting to make divisibility checks easier
+        # dp = [[num] for num in nums]  # Initialize subsets with single elements
+        # max_subset = []
 
-        for i in range(N):
-            for j in range(i):
-                if nums[i] % nums[j] == 0 and len(dp[j]) + 1 > len(dp[i]):
-                    dp[i] = dp[j] + [nums[i]]  # Expand subset
+        # for i in range(len(nums)):
+        #     for j in range(i):
+        #         if nums[i] % nums[j] == 0 and len(dp[j]) + 1 > len(dp[i]):
+        #             dp[i] = dp[j] + [nums[i]]  # Expand subset
 
-            if len(dp[i]) > len(max_subset):  # Track the longest subset
-                max_subset = dp[i]
+        #     if len(dp[i]) > len(max_subset):  # Track the longest subset
+        #         max_subset = dp[i]
 
-        return max_subset
+        # return max_subset
+
+        # Algorithm: ??? DP Tubulation
+        nums.sort()
+        dp = [[n] for n in nums] # dp[i] = longest start at i
+        res = []
+        for i in reversed(range(len(nums))):
+            for j in range(i + 1, len(nums)):
+                if nums[j] % nums[i] == 0:
+                    tmp = [nums[i]] + dp[j]
+                    dp[i] = tmp if len(tmp) > len(dp[i]) else dp[i]
+            res = dp[i] if len(dp[i]) > len(res) else res
+        return res
+
+        cache = {}
+        def dfs(i):
+            if i == len(nums): return []
+            if i in cache: return chache[i]
+
+            res = [nums[i]]
+            for j in range(i + 1, len(nums)):
+                if nums[j] % nums[i] == 0:
+                    tmp = [nums[i]] + dfs(j)
+                    if len(tmp) > len(res):
+                        res = tmp
+            cache[i] = res
+            return res
+        
+        res = []
+        for i in range(len(nums)):
+            tmp = dfs()
+            if len(tmp) > len(res):
+                res = tmp
+        return res
