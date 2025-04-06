d29096c0be29b2b21d2f25c2667788875c5722aa
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-06 12:39:23 -0400

    Time: 100 ms (100%), Space: 18.1 MB (59.42%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,58 +1,33 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        if n <= 1:
-            return nums
+        nums.sort()  # Sort the numbers to check divisibility easily
 
-        # Sort the stones (numbers) to process them in increasing order
-        nums.sort()
+        # res: Key = path length, Value = list of paths of that length
+        res = collections.defaultdict(list)
+        res[1].append([nums[0]])  # First number starts a path of length 1
+        max_len = 1  # Track the length of the longest path found
 
-        # Dictionary to store pyramids grouped by their height (length)
-        # Key: height (int)
-        # Value: list of pyramids (List[List[int]]) with that height
-        pyramids_by_height = collections.defaultdict(list)
+        for i in range(1, len(nums)):
+            num = nums[i]  # Current number to potentially add
+            added = False  # Track if 'num' was added to any path
 
-        # The first stone forms the first pyramid of height 1
-        pyramids_by_height[1].append([nums[0]])
-        max_height = 1
+            # Check existing paths, from longest to shortest
+            for length in range(max_len, 0, -1):
+                for path in res[length]:
+                    if num % path[-1] == 0:  # Check divisibility rule
+                        new_path = path + [num]  # Add 'num' to the path
+                        new_len = length + 1
+                        res[new_len].append(new_path)  # Record the new path
 
-        # Process the rest of the stones
-        for i in range(1, n):
-            current_num = nums[i]
-            was_added = False # Flag to check if current_num extended any pyramid
+                        if new_len > max_len:  # Update max length found
+                            max_len = new_len
 
-            # Try to place current_num on top of existing pyramids
-            # Check from the tallest pyramids downwards (prioritize extending taller ones)
-            for height in range(max_height, 0, -1):
-                # Look at all pyramids currently at this height
-                for pyramid in pyramids_by_height[height]:
-                    top_stone = pyramid[-1] # The last stone added to this pyramid
+                        added = True
+                        break  # Stop checking paths at this length
+                if added:
+                    break  # Stop checking shorter lengths
 
-                    # Check the rule: Can current_num go on top_stone?
-                    if current_num % top_stone == 0:
-                        # Yes! Extend this pyramid
-                        new_pyramid = pyramid + [current_num]
-                        new_height = height + 1
+            if not added:  # If 'num' couldn't extend any path
+                res[1].append([num])  # Start a new path of length 1
 
-                        # Add the new, taller pyramid to our records
-                        pyramids_by_height[new_height].append(new_pyramid)
-
-                        # Update the overall max_height if this is a new record
-                        if new_height > max_height:
-                            max_height = new_height
-
-                        # Mark that we added the stone and stop searching for this current_num
-                        was_added = True
-                        break # Stop checking other pyramids at this height
-                
-                if was_added:
-                    break # Stop checking pyramids of lower heights
-
-            # If current_num couldn't be added on top of ANY existing pyramid
-            if not was_added:
-                # It starts its own new pyramid of height 1
-                pyramids_by_height[1].append([current_num])
-
-        # Return any one of the pyramids with the maximum height
-        # The user's original code returned the first one found ([0])
-        return pyramids_by_height[max_height][0]
+        return res[max_len][0]  # Return any path with the maximum length
