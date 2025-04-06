7f57c778fa6db4bd5571b69810a6eb18640b3024
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-06 12:10:16 -0400

    Time: 102 ms (99.91%), Space: 18 MB (76.77%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,33 +1,23 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        N = len(nums)
-        if N <= 1:
-            return nums
-
-        nums.sort()
-
-        dp = [1] * N
-        parent = [-1] * N
-
-        max_height = 1
-        top_brick_i = 0
-
-        for i in range(N):
-            current_brick = nums[i]
-            for j in range(i):
-                previous_brick = nums[j]
-                if current_brick % previous_brick == 0:
-                    if dp[j] + 1 > dp[i]:
-                        dp[i] = dp[j] + 1
-                        parent[i] = j
-            if dp[i] > max_height:
-                max_height = dp[i]
-                top_brick_i = i
-
-        result = []
-        current_i = top_brick_i
-        while current_i != -1:
-            result.append(nums[current_i])
-            current_i = parent[current_i]
-
-        return result[::-1]
+        d = {}
+        nums = sorted(nums)
+        d[1] = [[nums[0]]]
+        max_level = 1
+        for num in nums[1:]:
+            inserted = False
+            for level in range(max_level, 0, -1):
+                for l in d[level]:
+                    if num % l[-1] == 0:
+                        if level + 1 > max_level:
+                            d[level + 1] = [l + [num]]
+                            max_level += 1
+                        else:
+                            d[level + 1] += [l + [num]]
+                        inserted = True
+                        break
+                if inserted:
+                    break
+                if level == 1 and l == d[level][-1]:
+                    d[1] += [[num]]
+        return d[max_level][0]
