f0d9bb5b4b45862a352a8d0ca90d14875ee92b2f
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-07 06:52:12 -0400

    Time: 483 ms (74.33%), Space: 17.8 MB (97.89%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -8,19 +8,17 @@ class Solution:
 
         # Algorithm: Dynamic Programming
         coins = sum(nums)
-        if coins % 2 != 0:
+        if coins & 1:
             return False
 
-        n = len(nums)
-        equal_share = coins // 2 
-        dp = [[False] * (equal_share + 1) for _ in range(n + 1)]
-        dp[0][0] = True
+        equal_share = coins // 2
+        dp = [False] * (equal_share + 1)
+        dp[0] = True
 
-        for i in range(1, n + 1):
-            num = nums[i - 1]
-            for j in range(equal_share + 1):
-                if j < num:
-                    dp[i][j] = dp[i - 1][j]
-                else:
-                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - num]
-        return dp[n][equal_share]
+        for coin in nums:
+            # Travers backward to avoid reusing the same num
+            for s in range(equal_share, coin - 1, -1):
+                # Either we already could reach s, or we can reach s - num and add num
+                dp[s] = dp[s] or dp[s - coin]
+
+        return dp[equal_share]
