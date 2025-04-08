339e1052e7ee2e23bfff0400fdbca1561b243c89
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-08 07:07:43 -0400

    Time: 3 ms (69.84%), Space: 17.7 MB (93.16%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,10 +1,14 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
+        # Algorithm: Reverse Traversal. HashSet
+        # Time Complexity O(n)
+        # Space Complexity O(n)
         seen = set()
 
         for i in range(len(nums) - 1, -1, -1):
             if nums[i] in seen:
-                return i // 3 + 1
+                return (i // 3) + 1
             else:
                 seen.add(nums[i])
         return 0
+        
