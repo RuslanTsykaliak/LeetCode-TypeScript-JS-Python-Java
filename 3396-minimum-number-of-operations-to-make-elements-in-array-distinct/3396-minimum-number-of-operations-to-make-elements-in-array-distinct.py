8e70e8487bc407b9978d11fc87ade641a8d254ef
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-08 06:57:20 -0400

    Time: 2 ms (74.36%), Space: 17.6 MB (93.16%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -4,7 +4,7 @@ class Solution:
 
         for i in range(len(nums) - 1, -1, -1):
             if nums[i] in seen:
-                return (i // 3) + 1
+                return i // 3 + 1
             else:
                 seen.add(nums[i])
         return 0
