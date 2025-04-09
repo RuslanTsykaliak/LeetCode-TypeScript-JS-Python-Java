af0f35eb9b34c2e1da41fc441224e990aa305ec2
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-09 09:15:05 -0400

    Time: 3 ms (52.54%), Space: 45 MB (8.47%) - LeetHub

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.java
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,25 +1,14 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < k) {
+        Set<Integer> uniqueHeights = new HashSet<>();
+        for (int n : nums) {
+            if (n < k) {
                 return -1;
             }
-
-            if (nums[i] > k) {
-                boolean found = false;
-                for (int j = 0; j < i; j++) {
-                    if (nums[i] == nums[j]) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
-                    count++;
-                }
+            if (n > k) {
+                uniqueHeights.add(n);
             }
         }
-        return count;
+        return uniqueHeights.size();
     }
 }
