4b6b54f47d8ac1db6898b85e3e22af85c93348b4
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-09 18:58:12 -0400

    Time: 0 ms (100%), Space: 17.7 MB (66.26%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,20 +6,39 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # Algorithm: Recursion
+        # # Algorithm: Recursion
 
-        # If both trees are null
-        if not p and not q:
-            return True
+        # # If both trees are null
+        # if not p and not q:
+        #     return True
         
-        # If one of the trees is empty but the other isn't
-        if not p or not q:
-            return False
+        # # If one of the trees is empty but the other isn't
+        # if not p or not q:
+        #     return False
         
-        # Compare the values of the current nodes
-        if p.val != q.val:
-            return False
+        # # Compare the values of the current nodes
+        # if p.val != q.val:
+        #     return False
 
-        # Recursively check the left and right subtrees
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # # Call the method to check the left and right subtrees
+        # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+        # Algorithm: Breath First Search
+
+        queue = deque([(p, q)])
+
+        while queue:
+            node1, node2 = queue.popleft()
+            
+            if not node1 and not node2:
+                continue
+                
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+                
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+
+        return True
+    
         
