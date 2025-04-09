29431978e7b84d3161c766110dddb767ae37279f
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-09 09:28:09 -0400

    Time: 54 ms (98.35%), Space: 17.8 MB (35.61%) - LeetHub

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.py
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.py
@@ -1,9 +1,18 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        unique = set()
+        # HashSet. Greedy
+
+        # If any number is smaller than k, we return -1
+        if min(nums) < k:
+            return -1
+        
+        uniqueHeights = set()
+
+        # We check all numbers in the array nums 
         for n in nums:
-            if n < k:
-                return -1
+            # Only track values that are greater than k
             if n > k:
-                unique.add(n)
-        return len(unique)
+                uniqueHeights.add(n)
+        # Each unique height above k will need the spell to reduce
+        return len(uniqueHeights)
+        
