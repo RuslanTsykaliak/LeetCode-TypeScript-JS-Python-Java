c306c78c11b476eada93533dc2b563b36db3543e
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-09 20:42:24 -0400

    Time: 64 ms (5.17%), Space: 52.5 MB (98.27%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,12 +1,16 @@
 function removeDuplicates(nums: number[]): number {
-    let k = 1;
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let r = 1;
 
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[i-1]) {
-            nums[k] = nums[i];
-            k++;
+        if (nums[i] !== nums[i - 1]) {
+            nums[r] = nums[i];
+            r++;
         }
     }
 
-    return k;
+    return r;
 };
