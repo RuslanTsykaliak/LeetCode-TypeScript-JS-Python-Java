67c947dc00cdc224acc77a5c6c43aa8e7aad2e84
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-13 09:04:05 -0400

    Time: 0 ms (100%), Space: 17.8 MB (44.07%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,17 +1,12 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = 10**9 + 7
+
         num_even_indices = (n + 1) // 2
         num_odd_indices = n // 2
+
         even_pos_choices = pow(5, num_even_indices, MOD)
         odd_pos_choices = pow(4, num_odd_indices, MOD)
+
         return (even_pos_choices * odd_pos_choices) % MOD
-
-        # res = 1
-        # rem = n % 2
-        # n -= rem
-        # res = pow(20, n // 2, 10**9 + 7)
-        # if rem == 1:
-        #     res *= 5
-        # return res % (10**9 + 7)
-
+        
