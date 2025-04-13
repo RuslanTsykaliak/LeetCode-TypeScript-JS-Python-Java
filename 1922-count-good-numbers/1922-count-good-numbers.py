173f48cb688214cbd8cf1372d5502290a79dfa77
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-12 20:31:08 -0400

    Time: 0 ms (100%), Space: 17.6 MB (93.38%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,15 +1,9 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        mod = 10**9 + 7
-
-        # use fast exponentiation to calculate x^y % mod
-        def quickmul(x: int, y: int) -> int:
-            ret, mul = 1, x
-            while y > 0:
-                if y % 2 == 1:
-                    ret = ret * mul % mod
-                mul = mul * mul % mod
-                y //= 2
-            return ret
-
-        return quickmul(5, (n + 1) // 2) * quickmul(4, n // 2) % mod
+        ans = 1
+        rem = n % 2
+        n -= rem
+        ans = pow(20, n // 2, 10**9 + 7)
+        if rem == 1:
+            ans *= 5
+        return ans % (10**9 + 7)
