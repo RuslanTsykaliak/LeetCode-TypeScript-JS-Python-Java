3642ba0deebe282d5480f1cc2426894aec80825b
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-14 07:40:52 -0400

    Time: 10 ms (77.35%), Space: 41.3 MB (75.9%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.java
+++ 1534-count-good-triplets/1534-count-good-triplets.java
@@ -1,28 +1,25 @@
 class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        // Input: three integers
-        // Output: count of triplets that follow these conditions:
-        // 0 <= i < j < k < arr.length
+        // Input: array and we have three integers a, b, c
+        // Output: count of good triplets
+        // Conditions:
+        // 0 <= i < j < k < arr.length. Satisfaid in Constraints:
         // |arr[i] - arr[j]| <= a
         // |arr[j] - arr[k]| <= b
         // |arr[i] - arr[k]| <= c
-        // where |x| denotes the absolute value. For example,
-        // Math.abs(-3 - 1) returns 4, and Math.abs(3 - 1) returns 2.
 
-        // Algorithms: Enumerate (Brute Force)
-        // Data Structures: Nested loops; use Math.abs for absolute value computations.
+        // Algorithm: Brute Force. Enumeratation
+        // Data Structures: array, counter, Math.abs() to count the absolute value
+        // Math.abs(-3 - 1); // +4 Math.abs(3 - 1) // +2
 
         int count = 0;
         int n = arr.length;
+
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
-                if (Math.abs(arr[i] - arr[j]) > a) {
-                    continue;
-                }
+                if (Math.abs(arr[i] - arr[j]) > a) continue;
                 for (int k = j + 1; k < n; k++) {
-                    if (Math.abs(arr[j] - arr[k]) > b) {
-                        continue;
-                    }
+                    if (Math.abs(arr[j] - arr[k]) > b) continue;
                     if (Math.abs(arr[i] - arr[k]) <= c) {
                         count++;
                     }
