e8d8787858dbf70bf63c8636e466b427c264f6fd
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-13 20:42:40 -0400

    Time: 181 ms (86.29%), Space: 17.7 MB (85.75%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.py
+++ 1534-count-good-triplets/1534-count-good-triplets.py
@@ -8,17 +8,20 @@ class Solution:
         # 3. (arr[j] - arr[k]) <= b
         # 4. (arr[i] - arr[k]) <= c
 
+        # Algorithm: Brute Force. Enumeration. Nested loop.
         count = 0
-
-        # Algorithm: Brute Force. Nested loop
-
-        l = len(arr)
-        for i in range(l):
-            for j in range(i + 1, l):
-                for k in range(j + 1, l):
-                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
-                        count += 1
+        n = len(arr)
+        for i in range(n - 2):
+            for j in range(i + 1, n -1):
+                if abs(arr[i] - arr[j]) > a:
+                    continue
+                for k in range(j + 1, n):
+                    if (
+                        abs(arr[j] - arr[k]) <= b
+                        and abs(arr[i] - arr[k]) <= c
+                        ):
+                            count += 1
         return count
 
-        
+
         
