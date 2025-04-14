9a70c69e514edaf373b1e872c540d64c5c05f1e0
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-14 07:47:58 -0400

    Time: 185 ms (82.26%), Space: 17.8 MB (45.16%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.py
+++ 1534-count-good-triplets/1534-count-good-triplets.py
@@ -1,23 +1,15 @@
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
-        # Input: three integers
-        # Output: count
-        # Conditions:
-        # 1. bigger/equal 0 and less than arr.length: 0 <= i < j < k < arr.length
-        # 2. (arr[i] - arr[j]) <= a
-        # 3. (arr[j] - arr[k]) <= b
-        # 4. (arr[i] - arr[k]) <= c
-
-        # Algorithm: Brute Force. Enumeration. Nested loop.
         count = 0
         n = len(arr)
+
         for i in range(n - 2):
-            for j in range(i + 1, n -1):
+            for j in range(i + 1, n - 1):
                 if abs(arr[i] - arr[j]) > a:
                     continue
                 for k in range(j + 1, n):
                     if abs(arr[j] - arr[k]) > b:
                         continue
                     if abs(arr[i] - arr[k]) <= c:
-                            count += 1
+                        count += 1 # count = count + 1
         return count
