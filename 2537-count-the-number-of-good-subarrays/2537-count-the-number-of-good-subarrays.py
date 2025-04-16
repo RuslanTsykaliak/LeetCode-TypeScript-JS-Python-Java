0509e93d0790bc3588aad845060e03c3c8da3501
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-15 23:25:13 -0400

    Time: 211 ms (10.68%), Space: 34.5 MB (45.31%) - LeetHub

---
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        same, right = 0, -1
+        cnt = Counter()
+        res = 0
+        for left in range(n):
+            while same < k and right + 1 < n:
+                right += 1
+                same += cnt[nums[right]]
+                cnt[nums[right]] += 1
+            if same >= k:
+                res += n - right
+            cnt[nums[left]] -= 1
+            same -= cnt[nums[left]]
+        return res
