5969779b6845b6e1b25d58e8e4ae06c482970554
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-17 08:38:33 -0400

    Time: 24 ms (25.33%), Space: 17.8 MB (80.5%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -1,108 +1,16 @@
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
+        # Input: An integer array nums and an integer k
+        # Output: The number of index-pairs (i, j) with i < j, nums[i] == nums[j] and (i * j) % k == 0.
+
+        # Algorithms: Brute Force. Nested loop
         count = 0
-        data = defaultdict(list)
-        for j, num in enumerate(nums):
-            if num in data:
-                for i in data[num]:
-                    if (i * j) % k == 0:
-                        count += 1
-            data[num].append(j)
-
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] != nums[j]:
+                    continue
+                if (i * j) % k == 0:
+                    count += 1
         return count
-
-
-        # Algorithm: Brute Force
-        # Time Complexity O(n^2)
-
-        # Input: array nums and an integer k
-        # Output: Count
-        # Coditions:
-        # nums[i] == nums[j]
-        # (i * j) is divisible by k
-
-        # count = 0
-        # n = len(nums)
-        # for i in range(n - 1):
-        #     for j in range(i + 1, n):
-        #         # Just copy past explanation part
-        #         if nums[i] != nums[j]: continue
-        #         if (i * j) % k == 0:
-        #             count += 1
-        # return count
-
-        #################################################
-        # What will be better solution?
-        # Hash Map
-
-        # Counter + Module inside of hash map
-        # count = 0
-        # map = defaultdict(Counter)
-
-        # for i, num in enumerate(nums):
-        #     for j in map[num]:
-        #         if (i * j) % k == 0:
-        #             count += map[num][j]
-        #     map[num][i % k] += 1
-
-        # return count
-
-
-        # Simpler Hash Map
-
-        # index_map = defaultdict(list)
-        # result = 0
-
-        # for i in range(len(nums)):
-        #     for j in index_map[nums[i]]:
-        #         if (i * j) % k == 0:
-        #             result += 1
-
-        #     index_map[nums[i]].append(i)
-
-        # return result
-
-
-        # Trying O(n)
-        # count = 0
-        # mod_groups = defaultdict(lambda: defaultdict(int))
-
-        # for i, num in enumerate(nums):
-        #     i_mod = i % k
-        #     for j_mod in range(k):
-        #         if (i_mod * j_mod) % k == 0:
-        #             count += mod_groups[num][j_mod]
-        #     mod_groups[num][i_mod] += 1
-        # return count
-
-
-        # def get_divisors(n):
-        #     divisors = set()
-        #     for i in range(1, int(math.isqrt(n)) + 1):
-        #         if n % i == 0:
-        #             divisors.add(i)
-        #             divisors.add(n // i)
-        #     return list(divisors)
         
-        # divisors = get_divisors(k)
-        
-        # # Use a defaultdict to track groups with default counts initialized for each divisor
-        # groups = defaultdict(lambda: {d: 0 for d in divisors})
-        
-        # result = 0
-        
-        # for i, num in enumerate(nums):
-        #     group = groups[num]
-        #     current_gcd = math.gcd(i, k)
-        #     t = k // current_gcd
-            
-        #     # Add the count of valid previous indices to the result
-        #     if t in group:
-        #         result += group[t]
-            
-        #     # Update the divisor counts for current index
-        #     for d in divisors:
-        #         if i % d == 0:
-        #             group[d] += 1
-        
-        # return result
