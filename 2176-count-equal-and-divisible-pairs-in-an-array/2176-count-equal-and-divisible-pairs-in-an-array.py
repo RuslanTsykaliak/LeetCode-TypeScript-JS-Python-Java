9d1f589384a28c3908f079cd7fbdfb3c896dd84e
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-17 08:43:38 -0400

    Time: 4 ms (97.83%), Space: 17.8 MB (48.17%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -4,13 +4,25 @@ class Solution:
         # Output: The number of index-pairs (i, j) with i < j, nums[i] == nums[j] and (i * j) % k == 0.
 
         # Algorithms: Brute Force. Nested loop
+        # Time Complexity O(n^2)
+        # count = 0
+        # n = len(nums)
+        # for i in range(n - 1):
+        #     for j in range(i + 1, n):
+        #         if nums[i] != nums[j]:
+        #             continue
+        #         if (i * j) % k == 0:
+        #             count += 1
+        # return count
+
+        # Algorithms: Single Pass. Hash Table
+        # Time Complexity O(n)
         count = 0
-        n = len(nums)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] != nums[j]:
-                    continue
+        index_map = defaultdict(list)
+        for j, num in enumerate(nums):
+            for i in index_map[num]:
                 if (i * j) % k == 0:
                     count += 1
+            index_map[num].append(j)
         return count
         
