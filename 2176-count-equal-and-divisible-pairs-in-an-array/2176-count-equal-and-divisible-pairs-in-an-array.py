7a0a0d8a44eacc3caacba7d1f61990bca11c237c
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-16 20:38:07 -0400

    Time: 7 ms (95%), Space: 17.7 MB (95.67%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
-        # Agorithm: Brute Force
+        # Algorithm: Brute Force
+        # Time Complexity O(n^2)
 
         # Input: array nums and an integer k
         # Output: Count
@@ -8,13 +9,27 @@ class Solution:
         # nums[i] == nums[j]
         # (i * j) is divisible by k
 
+        # count = 0
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         # Just copy past explanation part
+        #         if nums[i] != nums[j]: continue
+        #         if (i * j) % k == 0:
+        #             count += 1
+        # return count
+
+        #################################################
+        # What will be better solution?
+        # Hash Map
+
         count = 0
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                # Just copy past explanation part
-                if nums[i] != nums[j]: continue
+        map = defaultdict(list)
+
+        for i in range(len(nums)):
+            for j in map[nums[i]]:
                 if (i * j) % k == 0:
                     count += 1
+            map[nums[i]].append(i)
+
         return count
-        
