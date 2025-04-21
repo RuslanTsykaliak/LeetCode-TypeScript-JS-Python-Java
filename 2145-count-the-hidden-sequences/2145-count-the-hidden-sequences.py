caa982d5b72b6b21fe4404893db9715c2211ebb2
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-20 21:50:18 -0400

    Time: 107 ms (62.63%), Space: 32.2 MB (76.77%) - LeetHub

--- 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.py
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.py
@@ -1,14 +1,10 @@
 class Solution:
     def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
-        # x = y = cur = 0
-        # for d in differences:
-        #     cur += d
-        #     x = min(x, cur)
-        #     y = max(y, cur)
-        #     if y - x > upper - lower:
-        #         return 0
-        # return (upper - lower) - (y - x) + 1
-
-        ####
-        pfs = list(accumulate(differences, initial = 0))
-        return max(0, upper - lower - max(pfs) + min(pfs) + 1)
+        x = y = cur = 0
+        for d in differences:
+            cur += d
+            x = min(x, cur)
+            y = max(y, cur)
+            if y - x > upper - lower:
+                return 0
+        return (upper - lower) - (y - x) + 1
