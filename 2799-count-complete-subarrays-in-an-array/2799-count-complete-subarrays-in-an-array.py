0ef6a1d4130c5ded3621788b191633e68c30d67b
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-24 07:04:19 -0400

    Time: 577 ms (16.43%), Space: 18 MB (46.5%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -1,19 +1,19 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        n = len(nums)
-        total_unique = len(set(nums))
-        freq = {}
-        left = 0
+        # Nested loops
+        # set for pre-subarray distinct tracking
+        # Time Complexity O(n^2)
+        # Space Complexity O(n)
+
         count = 0
-
-        for right, v in enumerate(nums):
-            freq[v] = freq.get(v, 0) + 1
-
-            while len(freq) == total_unique:
-                count += n - right
-                freq[nums[left]] -= 1
-                if freq[nums[left]] == 0:
-                    del freq[nums[left]]
-                left += 1
-        
+        n = len(nums)
+        total_distinct = len(set(nums))
+        for i in range(n):
+            current_distinct = set()
+            for j in range(i, n):
+                current_distinct.add(nums[j])
+                if len(current_distinct) == total_distinct:
+                    count += 1
         return count
+
+        
