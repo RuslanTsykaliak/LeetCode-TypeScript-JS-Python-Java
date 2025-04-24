bc215074023646860f93345fe2dd01df3b72e2c9
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-24 07:11:01 -0400

    Time: 7 ms (95.1%), Space: 18 MB (76.05%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -5,15 +5,35 @@ class Solution:
         # Time Complexity O(n^2)
         # Space Complexity O(n)
 
+        # count = 0
+        # n = len(nums)
+        # total_distinct = len(set(nums))
+        # for i in range(n):
+        #     current_distinct = set()
+        #     for j in range(i, n):
+        #         current_distinct.add(nums[j])
+        #         if len(current_distinct) == total_distinct:
+        #             count += 1
+        # return count
+
+        # Sliding Window
+        # Dictionary freq to track window frequencies
+        # Time Compexity O(n) because of singe pass
+        # Space Complexity O(n)
+
         count = 0
         n = len(nums)
         total_distinct = len(set(nums))
-        for i in range(n):
-            current_distinct = set()
-            for j in range(i, n):
-                current_distinct.add(nums[j])
-                if len(current_distinct) == total_distinct:
-                    count += 1
+        freq = {}
+        left = 0
+        for right, v in enumerate(nums):
+            freq[v] = freq.get(v, 0) + 1
+            while len(freq) == total_distinct:
+                count += n - right
+                freq[nums[left]] -= 1
+                if freq[nums[left]] == 0:
+                    del freq[nums[left]]
+                left += 1
         return count
 
         
