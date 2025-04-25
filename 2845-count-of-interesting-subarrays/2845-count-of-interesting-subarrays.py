17ba63af34029cf898d6affd85ae44a0477c6e6c
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-24 20:25:48 -0400

    Time: 143 ms (67.82%), Space: 47.7 MB (51.15%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.py
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.py
@@ -1,17 +1,12 @@
 class Solution:
     def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
-        # Prefix Sum with Hash Map
-
-        current_remainder = 0
-        interesting_subarrays = 0
-        remainder_counter = {0: 1}
-
+        count = 0
+        prefix = 0
+        freq = defaultdict(int, {0: 1})
         for n in nums:
             if n % modulo == k:
-                current_remainder = (current_remainder + 1) % modulo
-            need = (current_remainder - k) % modulo
-            interesting_subarrays += remainder_counter.get(need, 0)
-
-            remainder_counter[current_remainder] = (remainder_counter.get(current_remainder, 0) + 1)
-
-        return interesting_subarrays
+                prefix = (prefix + 1) % modulo
+            need = (prefix - k) % modulo
+            count += freq[need]
+            freq[prefix] += 1
+        return count
