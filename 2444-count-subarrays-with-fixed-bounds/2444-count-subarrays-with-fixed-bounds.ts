ad6bba22b4d2204a2a695a318d1c0849768f5aca
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-25 21:43:59 -0400

    Time: 10 ms (50%), Space: 66.5 MB (100%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.ts
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.ts
@@ -1,19 +1,16 @@
 function countSubarrays(nums: number[], minK: number, maxK: number): number {
-    let answer = 0
-    let minKIdx = -1
-    let maxKIdx = -1
-    let outOfBoundsIdx = -1
+    let count = 0, minKIdx = -1, maxKIdx = -1, outOfBoundsIdx = -1;
     for (let i = 0; i < nums.length; i++) {
-        if(nums[i] > maxK || nums[i] < minK) {
-            outOfBoundsIdx = i
+        if (nums[i] > maxK || nums[i] < minK) {
+            outOfBoundsIdx = i;
         }
-        if(nums[i] === minK) {
-            minKIdx = i
+        if (nums[i] === minK) {
+            minKIdx = i;
         }
-        if(nums[i] === maxK) {
+        if (nums[i] === maxK) {
             maxKIdx = i
         }
-        answer += Math.max(0, Math.min(minKIdx, maxKIdx) - outOfBoundsIdx)
+        count += Math.max(0, Math.min(minKIdx, maxKIdx) - outOfBoundsIdx);
     }
-    return answer
+    return count;
 };
