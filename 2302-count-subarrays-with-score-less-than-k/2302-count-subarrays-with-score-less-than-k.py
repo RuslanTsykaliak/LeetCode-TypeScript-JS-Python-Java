0e058b91d256e54f1018e6c3a70bc283fb672a9c
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-27 21:30:35 -0400

    Time: 91 ms (99.59%), Space: 30.5 MB (61.29%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.py
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.py
@@ -1,18 +1,25 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        total = 0
-        window = 0
-        left = 0
-        count = 0
+        # Algorithm: Slidig Window
+        total = 0        # sum of elements in the current window [left…right]
+        window = 0       # length of the current window
+        left = 0         # left boundary of our sliding window
+        count = 0        # total number of valid subarrays
         n = len(nums)
 
         for right in range(n):
+            # include the element at index `right`
             total += nums[right]
             window += 1
-        
+
+            # shrink from the left until the score < k
+            # score = total * window
             while total * window >= k:
                 total -= nums[left]
                 window -= 1
                 left += 1
+
+            # all subarrays ending at `right` with lengths 1…window are valid
             count += window
+
         return count
