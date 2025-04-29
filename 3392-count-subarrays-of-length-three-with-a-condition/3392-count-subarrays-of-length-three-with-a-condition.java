86bc65d0dced0a2589326e5cf273f12cb6be1cda
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-04-29 07:49:53 -0400

    Time: 1 ms (100%), Space: 44.8 MB (96.55%) - LeetHub

--- 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.java
+++ 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.java
@@ -1,13 +1,33 @@
 class Solution {
     public int countSubarrays(int[] nums) {
         // Brute Force
-        int count = 0;
+        // int count = 0;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            // nums[i + 1] / 2
-            if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1]) {
+        // for (int i = 0; i < nums.length - 2; i++) {
+        //     // nums[i + 1] / 2
+        //     if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1]) {
+        //         count++;
+        //     }
+        // }
+
+        // return count;
+
+        int count = 0;
+        int l = 0;
+        int m = 1;
+        int r = 2;
+
+
+        while (r < nums.length) {
+            // doesn't pass nums = [-1,-4,-1,4]
+            // if ((nums[l] + nums[r]) == nums[m] / 2) {
+            // pass the test
+            if ((nums[l] + nums[r]) * 2 == nums[m]) {
                 count++;
             }
+            l++;
+            m++;
+            r++;
         }
 
         return count;
