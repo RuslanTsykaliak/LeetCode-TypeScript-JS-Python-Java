84b3fb66fd4061d8c2a4089cc2ae2ecab7dc618d
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-05-02 20:42:18 -0400

    Time: 23 ms (92.41%), Space: 18.3 MB (99.16%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,17 +1,20 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        def check(target):
-            top, bottom = 0, 0
-            for t, b in zip(tops, bottoms):
-                if t != target and b != target:
+        def check(candid):
+            # check O(N)
+            merge_to_bottom = merge_to_top = 0
+            for i in range(n):
+                if tops[i] != candid and bottoms[i] != candid:
                     return -1
-                elif t != target:
-                    top += 1
-                elif b != target:
-                    bottom += 1
-            return min(top, bottom)
+                elif tops[i] != candid:
+                    merge_to_top += 1
+                elif bottoms[i] != candid:
+                    merge_to_bottom += 1
+            return min(merge_to_top, merge_to_bottom)
         
-        res = check(tops[0])
-        if res != -1 or tops[0] == bottoms[0]:
-            return res
-        return check(bottoms[0])
+        n = len(tops)
+        rotations = check(tops[0])
+        if rotations != -1 or tops[0] == bottoms[0]:
+            return rotations
+        else:
+            return check(bottoms[0])
