8e8a6e1f86eadcfe9a889a716917e5c9d2fdff48
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-05-03 20:22:12 -0400

    Time: 19 ms (18.12%), Space: 24.4 MB (43.37%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
@@ -1,13 +1,27 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        count_map = defaultdict(int)
-        for a, b in dominoes:
-             # Normalize (a, b) to always have (min, max)
-             key = (min(a, b), max(a, b))
-             count_map[key] += 1
+        # # Hash Map
+        # # Runtime 53%
+        # count_map = defaultdict(int)
+        # for a, b in dominoes:
+        #      # Normalize (a, b) to always have (min, max)
+        #      key = (min(a, b), max(a, b))
+        #      count_map[key] += 1
         
+        # res = 0
+        # for c in count_map.values():
+        #     if c > 1:
+        #         res += c * (c - 1) // 2
+        # return res
+
+
+        # Hash Map 
+        
+        count = Counter()
         res = 0
-        for c in count_map.values():
-            if c > 1:
-                res += c * (c - 1) // 2
+        for a, b in dominoes:
+            key = (min(a, b), max(a, b))
+            res += count[key]
+            count[key] += 1
         return res
+
