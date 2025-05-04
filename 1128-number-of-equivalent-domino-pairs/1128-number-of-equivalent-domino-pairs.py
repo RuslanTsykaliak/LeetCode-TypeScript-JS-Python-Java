2eecf4c0be232db667e6f0fc06bbe1cd37a83b46
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-05-03 21:10:19 -0400

    Time: 0 ms (100%), Space: 24.3 MB (43.37%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
@@ -1,15 +1,36 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        # Algorithms: Normalization + Frequency Counting
-        # Time Complexity: O(n)
-        # Space Complexity: O(1)
+        # # Hash Map
+        # # Runtime 53%
+        # count_map = defaultdict(int)
+        # for a, b in dominoes:
+        #      # Normalize (a, b) to always have (min, max)
+        #      key = (min(a, b), max(a, b))
+        #      count_map[key] += 1
+        
+        # res = 0
+        # for c in count_map.values():
+        #     if c > 1:
+        #         res += c * (c - 1) // 2
+        # return res
 
-        freq = [0] * 100
-        total = 0
 
+        # Hash Map 
+        # Beats 37%
+        # count = Counter()
+        # res = 0
+        # for a, b in dominoes:
+        #     key = (min(a, b), max(a, b))
+        #     res += count[key]
+        #     count[key] += 1
+        # return res
+
+        # Beats 97 %
+        seen = [0] * 100
+        res = 0
         for a, b in dominoes:
-            key = a * 10 + b if a <= b else b * 10 + a
-            total += freq[key]
-            freq[key] += 1
+            val = a * 10 + b if a <= b else b * 10 + a
+            res += seen[val]
+            seen[val] += 1
+        return res
 
-        return total
