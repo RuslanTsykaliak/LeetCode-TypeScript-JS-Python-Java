7d7b45ad849e027357f78e143bff1c61a5128845
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-05-04 21:01:13 -0400

    Time: 0 ms (100%), Space: 18 MB (40.13%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,12 +1,19 @@
 class Solution:
     def numTilings(self, n: int) -> int:
+        # Dynamic Programming
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+
         MOD = 10**9 + 7
-        # Handle small n directly
-        if n == 0: return 0
+        # Base cases
         if n == 1: return 1
         if n == 2: return 2
+        if n == 0: return 0
 
-        dp = [1, 1, 2]  # dp[0], dp[1], dp[2]
-        for i in range(3, n + 1):
-            dp.append((2 * dp[i-1] + dp[i-3]) % MOD)
-        return dp[n]
+        dp = [1, 1, 2]
+
+        for i in range(3, n+1):
+            value = (dp[i-3] + 2 * dp[i-1]) % MOD
+            dp.append(value)
+        
+        return dp[-1]
