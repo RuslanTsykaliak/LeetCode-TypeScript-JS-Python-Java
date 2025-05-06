0814dc3e51db4c3fb1f620ee370e7aea7f0667d0
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-05-05 21:44:54 -0400

    Time: 3 ms (44.72%), Space: 17.7 MB (97.22%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,45 +1,25 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        # Normal explanation 
-        # we have an arr = [0,2,3,1]
-        # premutation will be equal to values:
-        # arr[0],arr[2], arr[3], arr[1]
-        # in numbers:
-        # 0, 3, 1, 2
+        # nums = [0, 2, 3, 1]
+        # nums[nums[i]] 
+        # idex 0 = 0
+        # idex 1 = 2
+        # idex 2 = 3
+        # idex 3 = 1
 
-        # How to realize it?
+        # ans = [0, 3, 1, 2]
 
-        # we can build an answer array
-        # ans = []
+        # Algorithms Array Traversal, Mathematical Econding
+        # Data Structure: Array
+        # Time Complexity & Space Complexity:
+        # Both solutions: O(n) time complexity
+        # Array Traversal: O(n) space complexity
+        # Mathematical Encoding: O(1) (in-pace modification)
+
+        # Array Traversal
+        ans = []
+        for i in range(len(nums)):
+            temp = nums[i]
+            ans.append(nums[temp])
+        return ans
         
-
-        # # next use for loop to add elements to the ans
-        # for i in range(len(nums)):
-        #     # assigne value of nums[i] to temp value
-        #     temp = nums[i]
-        #     # use temp value as index to add int to ans
-        #     ans.append(nums[temp])
-
-        # return ans
-
-        # Encoding
-        n = len(nums)
-        for i in range(n):
-            nums[i] = nums[i] + n * (nums[nums[i]] % n)
-        
-        for i in range(n):
-            nums[i] = nums[i] // n
-        
-        return nums
-        
-
-
-
-
-
-
-
-
-
-
-
