7f1028af4b4c39df97e00fbd1980bd3edbb9f064
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-05-05 20:36:00 -0400

    Time: 0 ms (100%), Space: 18 MB (55.24%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -0,0 +1,34 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        # Normal explanation 
+        # we have an arr = [0,2,3,1]
+        # premutation will be equal to values:
+        # arr[0],arr[2], arr[3], arr[1]
+        # in numbers:
+        # 0, 3, 1, 2
+
+        # How to realize it?
+
+        # we can build an answer array
+        ans = []
+        
+
+        # next use for loop to add elements to the ans
+        for i in range(len(nums)):
+            # assigne value of nums[i] to temp value
+            temp = nums[i]
+            # use temp value as index to add int to ans
+            ans.append(nums[temp])
+
+        return ans
+
+
+
+
+
+
+
+
+
+
+
