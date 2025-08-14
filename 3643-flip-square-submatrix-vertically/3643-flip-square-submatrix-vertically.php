3525d32ed96a68341d60ce7b5f9dc2cd533111b2
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-08-14 18:54:11 -0400

    Time: 0 ms (100%), Space: 20.3 MB (100%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.php
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.php
@@ -8,13 +8,15 @@ class Solution {
      * @return Integer[][]
      */
     function reverseSubmatrix($grid, $x, $y, $k) {
-        # The goal is to reverse rows in the square
+        // The goal is to replace the top row with the bottom row inside of the grid and then return the updated grid.
+        // We can declare top and bottom variables to hold row indices from top to button. Then run a nested while and for loops:
+        // the first one will update the rows, and the second will update the numbres inside of the grid.
 
-        # We will need to indentify the top and bottom rows
+        // Declare $top and $bottom variables
         $top = $x;
         $bottom = $x + $k - 1;
 
-        # We can use loop to swap rows from top to bottom
+        // Run the nested loop to swap rows
         while ($top < $bottom) {
             for ($i = 0; $i < $k; $i++) {
                 $temp = $grid[$top][$y + $i];
