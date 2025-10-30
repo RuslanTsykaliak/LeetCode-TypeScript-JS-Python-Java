21ecdae59656c8515bac8c04971e935a7bfd1019
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-10-29 21:51:18 -0400

    Time: 8 ms (100%), Space: 27.6 MB (100%) - LeetHub

---
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.php
@@ -0,0 +1,18 @@
+class Solution {
+
+    /**
+     * @param Integer[] $target
+     * @return Integer
+     */
+    function minNumberOperations($target) {
+        $currentMax = 0;
+        $result = 0;
+        foreach ($target as $value) {
+            if ($value > $currentMax) {
+                $result += $value - $currentMax;
+            }
+            $currentMax = $value;
+        }
+        return $result;
+    }
+}
