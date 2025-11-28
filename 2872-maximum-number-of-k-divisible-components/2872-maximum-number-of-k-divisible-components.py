6e7cb13419d2c08dd2c0b7588cf01b26892ddc4c
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2025-11-27 20:38:36 -0500

    Time: 155 ms (31.88%), Space: 32.5 MB (90.58%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
@@ -2,25 +2,42 @@ class Solution:
     def maxKDivisibleComponents(
         self, n: int, edges: List[List[int]], values: List[int], k: int
     ) -> int:
-        N = len(edges) + 1
-        graph = {i: set([]) for i in range(N)}
-        for u, v in edges:
-            graph[u].add(v)
-            graph[v].add(u)
-        self.res = 0
-        values = [v % k for v in values]
+        if n < 2:
+            return 1
+        component_count = 0
+        graph = defaultdict(list)
+        in_degree = [0 for _ in range(n)]
 
-        @cache
-        def dfs(node, parent):
-            val = values[node]
-            for child in graph[node]:
-                if child == parent:
+        # Build the graph and calculate in-degrees
+        for node1, node2 in edges:
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+            in_degree[node1] += 1
+            in_degree[node2] += 1
+
+        # Initialize the queue with nodes having in-degree of 1 (leaf nodes)
+        queue = deque(node for node in range(n) if in_degree[node] == 1)
+
+        while queue:
+            current_node = queue.popleft()
+            in_degree[current_node] -= 1
+            add_value = 0
+
+            # Check if the current node's value is divisible by k
+            if values[current_node] % k == 0:
+                component_count += 1
+            else:
+                add_value = values[current_node]
+
+            # Propagate the value to the neighbor nodes
+            for neighbor_node in graph[current_node]:
+                if in_degree[neighbor_node] == 0:
                     continue
-                val += dfs(child, node)
-            val = val % k
-            if val == 0:
-                self.res += 1
-            return val
+                in_degree[neighbor_node] -= 1
+                values[neighbor_node] += add_value
 
-        dfs(0, -1)
-        return self.res
+                # If the neighbor node's in-degree becomes 1, add it to the queue
+                if in_degree[neighbor_node] == 1:
+                    queue.append(neighbor_node)
+
+        return component_count
