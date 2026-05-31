c368b9718cf5fd11b2f7bf5ae3664ebca7c79ce6
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2026-05-30 21:46:41 -0400

    Time: 26 ms (81.52%), Space: 107.5 MB (47.81%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids); // Sort by mass in ascending order
+        long currentMass = mass; // Prevent integer overflow
+        for (int asteroid : asteroids) {
+            // Traverse the asteroids in order, attempt to destroy and update mass or return the result
+            if (currentMass < asteroid) {
+                return false;
+            }
+            currentMass += asteroid;
+        }
+        return true; // Successfully destroyed all asteroids
+    }
+}
