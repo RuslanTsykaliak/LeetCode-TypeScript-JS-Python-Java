a5e0cff3d738aedc10f3f2ef73caad91a8ffcc5f
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2026-08-28 20:44:51 -0400

    Time: 191 ms (9.85%), Space: 158 MB (65.15%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        for (int i =0;i<nums.length;i++){
-            pq.add(new int[]{nums[i],i});
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(new int[] { nums[i], i });
         }
-        while (!pq.isEmpty()){
+        while (!pq.isEmpty()) {
             List<Integer> l = new ArrayList<>();
-            List<Integer> idx  = new ArrayList<>();
+            List<Integer> idx = new ArrayList<>();
             int[] ele = pq.remove();
             l.add(ele[0]);
             idx.add(ele[1]);
-            while (!pq.isEmpty() && pq.peek()[0]<=ele[0]+limit){
+            while (!pq.isEmpty() && pq.peek()[0] <= ele[0] + limit) {
                 ele = pq.remove();
-            l.add(ele[0]);
-            idx.add(ele[1]);
+                l.add(ele[0]);
+                idx.add(ele[1]);
             }
             Collections.sort(idx);
-            for (int i=0;i<l.size();i++){
-                nums[idx.get(i)]=l.get(i);
+            for (int i = 0; i < l.size(); i++) {
+                nums[idx.get(i)] = l.get(i);
             }
         }
         return nums;
